e12ef61ce5e1aa10be028eea14827ac97be96af0
Abdul Wasay Imran <abdulwasay905@gmail.com>
2026-03-20 03:37:50 +0500

    dvqevasd

--- setup.sh
+++ setup.sh
@@ -5,31 +5,4 @@ python -m venv .venv
 .\.venv\Scripts\activate
 
 # Install request for fecthing html web page
-pip install requests
-
-# Install Langraph this provides nodes and routing iirc
-pip install -U langgraph
-
-# Install Langchain this porvides llm and some other stuff
-pip install -U langchain
-
-# Installing the OpenAI integration
-pip install -U "langchain[openai]"
-
-# Installing the Anthropic integration
-pip install -U langchain-anthropic
-
-# Install Google Gemini integration
-pip install -U "langchain[google-genai]"
-
-# install deepagents
-pip install deepagents 
-
-# install dotenv 
-pip install python-dotenv
-
-# for interactive cli, not good as ink but for now better than nothing 
-pip install rich prompt_toolkit
-
-# ionstall selenium and beautifulsoup for web scraping
-pip install selenium beautifulsoup4
+pip install requests langgraph langchain "langchain[openai]" langchain-anthropic "langchain[google-genai]" deepagents  python-dotenv rich prompt_toolkit selenium beautifulsoup4
